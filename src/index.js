62903ea72c484ea51de7e234bc2ea8bc618c2060
Spencer Penrod <spencerpenrod@gmail.com>
2023-03-27 15:45:02 -0600

    Feat: Add tab switching logic to website

--- src/index.js
+++ src/index.js
@@ -5,6 +5,41 @@ import fillContactPage from './contactPage';
 import fillMenu from './menu';
 
 createNavbar();
-// fillHomePage();
-// fillContactPage();
-fillMenu();
+
+const nav = {
+  menu: document.querySelector('.menu-nav'),
+  home: document.querySelector('.home-nav'),
+  contact: document.querySelector('.contact-nav'),
+};
+
+const resetPage = () => {
+  Object.values(nav).forEach((item) => item.classList.remove('active-tab'));
+  const dynamicContent = document.querySelector('.menu + div');
+  if (dynamicContent) dynamicContent.remove();
+};
+
+const activate = (navItem) => {
+  resetPage();
+  navItem.classList.add('active-tab');
+};
+
+const activateHome = () => {
+  activate(nav.home);
+  fillHomePage();
+};
+
+const activateContact = () => {
+  activate(nav.contact);
+  fillContactPage();
+};
+
+const activateMenu = () => {
+  activate(nav.menu);
+  fillMenu();
+};
+
+nav.home.addEventListener('click', activateHome);
+nav.contact.addEventListener('click', activateContact);
+nav.menu.addEventListener('click', activateMenu);
+
+activateHome();
